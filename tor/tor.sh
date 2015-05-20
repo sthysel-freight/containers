74a27ba25cd8b592e9fddfff25a5f9e76d3a2a4b
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-05-20 15:10:02 +0800

    tor browser runscript

---
+++ tor/tor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xhost +
+
+docker run -it --rm \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e DISPLAY=unix$DISPLAY \
+  -v /dev/snd:/dev/snd \
+  --privileged \
+  --name torb \
+  sthysel/torb
