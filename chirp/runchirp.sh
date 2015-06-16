d66c387cda9a5ffdfad6cf01ebd9bf9094e3ff13
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-06-16 22:46:22 +0800

    Sample csv and img

--- chirp/runchirp.sh
+++ chirp/runchirp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xhost +
-docker run -it --rm --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=unix$DISPLAY --name=chirp --hostname=chirp sthysel/chirp
+docker run -it --rm --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/bus/usb:/dev/bus/usb -v ${PWD}/data:/data -e DISPLAY=unix$DISPLAY --name=chirp --hostname=chirp sthysel/chirp
