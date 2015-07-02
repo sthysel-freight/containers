3bc131a9ba320f753eb52600ced2f53876ad48c8
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-07-02 21:56:17 +0800

    Working build environment

--- delta3d/develop.sh
+++ delta3d/develop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DELTA3D_VERSION=REL-2.8.0
+CONTAINER_NAME=delta3dbuilder
 
 have_tool(){
   tool_path=$(which $1)
@@ -19,7 +20,9 @@ have_tool docker-compose
 
 mkdir -p src/${DELTA3D_VERSION}
 echo "Checkout ${DELTA3D_VERSION}"
-cd src && svn checkout svn://svn.code.sf.net/p/delta3d/code/tags/${DELTA3D_VERSION}
+( cd src && svn checkout svn://svn.code.sf.net/p/delta3d/code/tags/${DELTA3D_VERSION} )
 
-echo "Raising build container"
-docker-compose up
+echo "Building container"
+docker build -t sthysel/delta3d .
+
+docker run -it --rm --name ${CONTAINER_NAME} --hostname ${CONTAINER_NAME} -v ${PWD}/src:/src/ sthysel/delta3d /bin/bash
