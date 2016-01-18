869067da398362537c1906cef81f932fb3a16ed4
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-01-18 21:42:35 +0800

    Adds sample flask app

---
+++ flask/app/square.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template("square.html")
+
+@app.route('/square/submit', methods = ['POST'])
+def square_submit():
+   x = int(request.form.get('x', 0))
+   res = x * x
+   return "{} squared is {}".format(x, res)
+
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=8080, debug=True)
